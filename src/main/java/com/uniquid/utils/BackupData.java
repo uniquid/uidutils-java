133767977e4aa394b4de44dde16d641ca28eb017
bea.formai@gmail.com <bea.formai@gmail.com>
2017-09-05 17:47:57 +0200

    add BackupData class to backup wallet data

---
+++ src/main/java/com/uniquid/utils/BackupData.java
@@ -0,0 +1,38 @@
+package com.uniquid.utils;
+
+public class BackupData {
+	
+	private String mnemonic;
+	private String name;
+	private long creationTime;
+
+	public String getMnemonic() {
+		return mnemonic;
+	}
+
+
+	public void setMnemonic(String mnemonic) {
+		this.mnemonic = mnemonic;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public long getCreationTime() {
+		return creationTime;
+	}
+
+
+	public void setCreationTime(long creationTime) {
+		this.creationTime = creationTime;
+	}
+
+}

75fab533c96b5aa5c4c9f60068228f47307b08b6
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2017-09-28 16:23:26 +0200

    moved connector part into untils project

---
+++ src/main/java/com/uniquid/connector/Connector.java
@@ -0,0 +1,28 @@
+package com.uniquid.connector;
+
+/**
+ * Connector interface hides the specifics of a communication protocol and allows the library to works independently from
+ * a particular implementation.
+ */
+public interface Connector {
+
+	/**
+	 * Starts the connector
+	 */
+	public void start() throws ConnectorException;
+	
+	/**
+	 * Stop the connector
+	 */
+	public void stop() throws ConnectorException;
+	
+	/**
+	 * Listens for a connection to be made to this connector and accepts it. The method blocks until a
+	 * connection is made
+	 * 
+	 * @return {@link EndPoint} the endpoint that wrap the communication with the User.
+	 * @throws ConnectorException in case a problem occurs.
+	 */
+	public EndPoint accept() throws ConnectorException, InterruptedException;
+	
+}
---
+++ src/main/java/com/uniquid/connector/EndPoint.java
@@ -0,0 +1,27 @@
+package com.uniquid.connector;
+
+import com.uniquid.messages.UniquidMessage;
+
+/**
+ * Represents a communication between a Provider and a User
+ */
+public interface EndPoint {
+	
+	/**
+	 * Returns the {@link ProviderRequest} performed by the User
+	 * @return the {@link ProviderRequest} performed by the User
+	 */
+	public UniquidMessage getInputMessage();
+
+	/**
+	 * Returns the {@link ProviderResponse} to be returned to the User
+	 * @return the {@link ProviderResponse} to be returned to the User
+	 */
+	public UniquidMessage getOutputMessage();
+	
+	/**
+	 * Closes this instance sending all the communication to the User.
+	 */
+	public void flush() throws ConnectorException;
+
+}

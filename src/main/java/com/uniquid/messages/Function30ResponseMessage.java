f3a3cf3b7a6e34efb1eabf9c8c0061ff27de1ff6
valentin <valentin.zakharov1@gmail.com>
2018-10-14 00:42:17 +0300

    COR-527: Fixed contract txid bug, small refactoring

---
+++ src/main/java/com/uniquid/messages/Function30ResponseMessage.java
@@ -0,0 +1,23 @@
+package com.uniquid.messages;
+
+public class Function30ResponseMessage extends FunctionResponseMessage {
+
+    private String txid;
+    private int txidError;
+
+    public String getTxid() {
+        return txid;
+    }
+
+    public void setTxid(String txid) {
+        this.txid = txid;
+    }
+
+    public int getTxidError() {
+        return txidError;
+    }
+
+    public void setTxidError(int txidError) {
+        this.txidError = txidError;
+    }
+}

bbd974c0faadfc4c4d0a3cb6b02757e335d83e55
Beatriz84 <bea.formai@gmail.com>
2019-06-07 17:30:18 +0200

    implemented method to download a file from a url

--- src/main/java/com/uniquid/utils/HttpUtils.java
+++ src/main/java/com/uniquid/utils/HttpUtils.java
@@ -10,10 +10,10 @@ package com.uniquid.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.annotation.Nullable;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.stream.Collectors;
 
@@ -22,6 +22,9 @@ public class HttpUtils {
     private static Logger LOGGER = LoggerFactory.getLogger(HttpUtils.class);
 
     private static final String USER_AGENT = "UNIQUID-UTILS-0.1";
+
+    private static final int CONNECTION_TIMEOUT = 10 *1000;
+    private static final int READ_TIMEOUT = 15 *1000;
 
     public static <T> T sendDataWithPost(URL url, DataProvider<T> dataProvider) throws Exception {
 
@@ -93,6 +96,62 @@ public class HttpUtils {
 
         }
 
+    }
+
+    public static <T> T downloadFile(URL url, @Nullable String path, ResponseDecoder<T> responseDecoder) throws Exception {
+
+        try {
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // optional default is GET
+            connection.setRequestMethod("GET");
+
+            // add request header
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+            connection.setReadTimeout(READ_TIMEOUT);
+
+            String fileName = "";
+            String disposition = connection.getHeaderField("Content-Disposition");
+            if (disposition != null) {
+                // extracts file name from header field
+                int index = disposition.indexOf("filename=");
+                if (index > 0) {
+                    fileName = disposition.substring(index + 10,
+                            disposition.length() - 1);
+                }
+            } else {
+                throw new Exception("Exception");
+            }
+            String saveFilePath = path != null ? path + File.separator + fileName : fileName;
+            // opens an output stream to save into file
+            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+            InputStream inputStream = connection.getInputStream();
+
+            int bytesRead;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            if (connection.getResponseCode() == responseDecoder.getExpectedResponseCode()) {
+                return responseDecoder.manageResponse(saveFilePath);
+            } else {
+                return responseDecoder.manageUnexpectedResponseCode(connection.getResponseCode(),
+                        connection.getResponseMessage(),
+                        outputStream.toString());
+            }
+
+        } catch (SocketTimeoutException e) {
+            LOGGER.error("Unexpected Exception", e);
+            throw new Exception("Unexpected Exception", e);
+        }
     }
 
 }
